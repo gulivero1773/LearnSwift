b006a6769f67fdaa486150ce68f631630cbde486
gulivero1773 <61082418+gulivero1773@users.noreply.github.com>
2020-03-10 13:23:59 +0200

    view1

---
+++ view1/view1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  view1
+//
+//  Created by Mykola Buhaiov on 10.03.2020.
+//  Copyright © 2020 Mykola Buhaiov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+
+}
+
