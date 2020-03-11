7f4720e081aa1acc13e0528286680dc805e26ac5
Mykola <61082418+gulivero1773@users.noreply.github.com>
2020-03-11 16:05:54 +0200

    view1

---
+++ singlview1/singlview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  singlview
+//
+//  Created by Mykola Buhaiov on 11.03.2020.
+//  Copyright © 2020 Mykola Buhaiov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
