b7a865fc4735e9e2f8b4d9000c5b004af3d1d580
Mykola <61082418+gulivero1773@users.noreply.github.com>
2020-03-11 18:21:46 +0200

    Core_Data

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var names: [String] = []
+    var people: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +29,7 @@ class ViewController: UIViewController {
             guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
                 return
             }
-            self.names.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
@@ -37,16 +39,39 @@ class ViewController: UIViewController {
         present(alert, animated: true)
         
     }
+    
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        person.setValue(name, forKey: "name")
+        
+        
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error  as NSError {
+            print("НЕ могу записать. \(error), \(error.userInfo)")
+            
+        }
+    }
+    
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let person = people[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = person.value(forKey: "name") as? String
         return cell 
     }
     
