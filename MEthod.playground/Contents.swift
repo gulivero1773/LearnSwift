f3322a39e75cc15ac84c86834c342ef7c3062cd6
Mykola <61082418+gulivero1773@users.noreply.github.com>
2020-03-11 21:05:41 +0200

    method2

--- MEthod.playground/Contents.swift
+++ MEthod.playground/Contents.swift
@@ -62,3 +62,55 @@ ovenLight.next()
 ovenLight
 ovenLight.next()
 ovenLight
+
+
+
+
+struct LevelTracker {
+     static var highestUnlockedLevel = 1
+    var currentLevel = 1
+    
+    static func unlock(_ level: Int) {
+        if level > highestUnlockedLevel {
+            highestUnlockedLevel = level
+        }
+        
+        
+    }
+    static func isUnlocked(_ level: Int) -> Bool {
+        return level <= highestUnlockedLevel
+    }
+    mutating func advance( to level: Int) -> Bool {
+        if LevelTracker.isUnlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+    func complete(level: Int){
+        LevelTracker.unlock(level + 1)
+        tracker.advance(to: level + 1)
+        
+    }
+    init(name: String) {
+        playerName = name
+    }
+}
+var player = Player(name: "Mykola")
+player.complete(level: 1)
+print("Максимальный уровень \(LevelTracker.highestUnlockedLevel)")
+
+player = Player(name: "Adrey")
+if player.tracker.advance(to: 2) {
+    print("Игорк на уровне 2")
+    
+} else {
+    print("Уровень 2 еще не разблокирован")
+}
+
