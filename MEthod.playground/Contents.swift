d5c4de7e17b376e384a93bcd8dc8b0d09d91ea84
Mykola <61082418+gulivero1773@users.noreply.github.com>
2020-03-11 20:35:01 +0200

    Method1

--- MEthod.playground/Contents.swift
+++ MEthod.playground/Contents.swift
@@ -23,14 +23,42 @@ counter.reset()
 counter.count
 
 
-struct point {
+struct Point {
     var x = 0.0, y = 0.0
     func isToTheRightOf(x: Double) -> Bool {
         return self.x > x
     }
+    mutating func moveBy( x deltaX: Double, y deltaY: Double){
+self = Point(x: x + deltaX, y: y + deltaY)
+    }
 }
 
-let somePoint = point(x: 4.0, y: 5.0)
+var somePoint = Point(x: 4.0, y: 5.0)
 if somePoint.isToTheRightOf(x: 1.0) {
     print("эта точка находится справа от линии где х = 1.0")
 }
+
+somePoint.moveBy(x: 2.0, y: 3.0)
+print("\(somePoint)")
+
+enum TriStateSwitch {
+    case off, low, high
+    mutating func next() {
+        switch self {
+        case .off:
+            self = .low
+      case .low:
+        self = .high
+            case .off:
+            self = .low
+            case .high:
+            self = .off
+        }
+    }
+}
+
+var ovenLight = TriStateSwitch.low
+ovenLight.next()
+ovenLight
+ovenLight.next()
+ovenLight
