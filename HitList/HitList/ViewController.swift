79adeb28a8314a00fc95037faaf4235343596d7a
Mykola <61082418+gulivero1773@users.noreply.github.com>
2020-03-11 16:40:42 +0200

    Data_Core

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Список "
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
     }
-
-
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Новое имя", message: "Добавьте новое имя", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Сохранить", style: .default) { [unowned self] (action) in
+            guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
+                return
+            }
+            self.names.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+        
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell 
+    }
+    
+    
+}
