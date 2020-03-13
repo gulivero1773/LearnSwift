5b4f8bb6969376e61a74d52e9c6df56598e43a1c
Mykola <61082418+gulivero1773@users.noreply.github.com>
2020-03-13 16:32:26 +0200

    egg_cook

---
+++ Egg_cook/Egg_cook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Egg_cook
+//
+//  Created by Mykola Buhaiov on 13.03.2020.
+//  Copyright © 2020 Mykola Buhaiov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
