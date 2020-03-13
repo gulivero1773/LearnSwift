4b7972d855f20eca25ff1bbc39c3d3b0b984b639
Mykola <61082418+gulivero1773@users.noreply.github.com>
2020-03-13 13:44:23 +0200

    commit

--- Initial.playground/Contents.swift
+++ Initial.playground/Contents.swift
@@ -81,3 +81,72 @@ breakfastList[0].purchased = true
 for item in breakfastList {
     print(item.description)
 }
+
+let mayBeNumber = "123"
+if let number = Int(mayBeNumber) {
+    print("\(mayBeNumber) \(number)")
+} else {
+    print("\(mayBeNumber)")
+}
+
+
+struct Animal {
+    let special: String
+    init?(special: String) {
+        if special.isEmpty { return nil }
+        self.special = special
+    }
+}
+
+let someCreature = Animal(special: "Жираф")
+if let giraffe = someCreature {
+    print("Мы не инеци \(giraffe.special)")
+}
+
+
+let anonymousCreature = Animal(special: "")
+if anonymousCreature == nil {
+    print("не из")
+}
+
+
+
+enum TemperatureUnit {
+    case kelvin, celsius, fahrenheit
+    init?(symbol: Character) {
+        switch symbol {
+        case "F":
+            self = .fahrenheit
+        case "K":
+        self = .kelvin
+        case "C":
+            self = .celsius
+        default:
+            return nil
+    }
+}
+}
+let fahrengeitUnit = TemperatureUnit(symbol: "F")
+if fahrengeitUnit != nil {
+    print("Эта единица измерения")
+}
+let unknowUnit = TemperatureUnit(symbol: "X")
+if unknowUnit == nil {
+    print("не знаем")
+}
+
+/*
+
+enum TemperatureUnit: Character {
+    case kelvin = "K", celsius = "C", fahrenheit = "F"
+  
+}
+let fahrengeitUnit = TemperatureUnit(rawValue: "F")
+if fahrengeitUnit != nil {
+    print("Эта единица измерения")
+}
+let unknowUnit = TemperatureUnit(rawValue: "X")
+if unknowUnit == nil {
+    print("не знаем")
+}
+*/
