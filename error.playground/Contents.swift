3107c01765899b46badadff2722b64991e922836
Mykola <61082418+gulivero1773@users.noreply.github.com>
2020-03-17 19:36:25 +0200

    common

--- error.playground/Contents.swift
+++ error.playground/Contents.swift
@@ -15,7 +15,7 @@ struct Item {
 class VendingMachine {
     var inventory = [
         "Candy": Item(price: 12, count: 7),
-        "chips": Item(price: 10, count: 4),
+        "Chips": Item(price: 10, count: 4),
         "Pretzels": Item(price: 7, count: 11)]
     var coinsDeposited = 0
     
@@ -47,13 +47,50 @@ let favoriteSnacks = [
 
 func buyFavoriteSnack(person: String, vedingMachine: VendingMachine) throws {
     let snackName = favoriteSnacks[person] ?? "Candy"
-    try vendingMachine.vend(itemNamed: snackName)
+    try vendingMachine.vend(itemName: snackName)
+}
+
+var vendingMachine = VendingMachine()
+vendingMachine.coinsDeposited = 8
+
+do {
+    try buyFavoriteSnack(person: "Alice", vedingMachine: vendingMachine)
+} catch VendingMachineError.invalideSelection {
+    print("")
+} catch VendingMachineError.outOfStock {
+    print("")
+} catch VendingMachineError.insufficientFunds(let coinsNeed) {
+    print("недос")
 }
 
 
+func someThowingFunc() throws -> Int {
+    return 0
+}
+
+let x = try? someThowingFunc()
+
+let y: Int?
+do {
+    y = try someThowingFunc()
+} catch {
+    y = nil
+}
 
 
+//let photo = try! loadImage(atPath: "l;k")
 
 
-
-
+func processFile(fileName: String) throws {
+    if exists(fileName) {
+        let file = open(fileName)
+        defer {
+            close(file)
+        }
+    }
+    while let line = try file.readline() {
+        //
+    }
+    
+    // close(file)
+}
